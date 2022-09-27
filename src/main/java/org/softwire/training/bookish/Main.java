abf78cdb96bdc01d7933bdf16cb0f8bd2e29c604
Elza Contiero <contieroelza@gmail.com>
2022-09-27 16:49:56 +0100

    Add JDBC and JDBI examples.

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -1,10 +1,10 @@
 package org.softwire.training.bookish;
 
 import org.jdbi.v3.core.Jdbi;
+import org.softwire.training.bookish.models.database.Book;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.List;
 
 
 public class Main {
@@ -12,8 +12,8 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         String hostname = "localhost";
         String database = "bookish";
-        String user = "bookish";
-        String password = "bookish";
+        String user = "root";
+        String password = "So, you think you can guess my password, do you?";
         String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
 
         jdbcMethod(connectionString);
@@ -21,27 +21,44 @@ public class Main {
     }
 
     private static void jdbcMethod(String connectionString) throws SQLException {
+        String query = "select BookID, title, author, isbn, category from Books";
         System.out.println("JDBC method...");
 
-        // TODO: print out the details of all the books (using JDBC)
         // See this page for details: https://docs.oracle.com/javase/tutorial/jdbc/basics/processingsqlstatements.html
-
         Connection connection = DriverManager.getConnection(connectionString);
-
-
-
+        try (Statement stmt = connection.createStatement()) {
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                int bookID = rs.getInt("BookId");
+                String title = rs.getString("title");
+                String author = rs.getString("author");
+                String isbn = rs.getString("isbn");
+                String category = rs.getString("category");
+                System.out.println("BookId is: " + bookID + " Title is: " + title +
+                        " Author is: " + author + " ISBN: " + isbn + " category: " + category);
+            }
+            rs.close(); // Good to close the ResultSet once done.
+            stmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
     }
 
     private static void jdbiMethod(String connectionString) {
         System.out.println("\nJDBI method...");
 
-        // TODO: print out the details of all the books (using JDBI)
         // See this page for details: http://jdbi.org
-        // Use the "Book" class that we've created for you (in the models.database folder)
-
         Jdbi jdbi = Jdbi.create(connectionString);
 
+        List<Book> books = jdbi.withHandle(handle -> {
+           return handle.createQuery("select BookID, title, author, isbn, category from Books")
+                   .mapToBean(Book.class)
+                   .list();
+        });
 
-
+        for(Book b: books) {
+            System.out.println(b);
+        }
     }
 }
--- src/main/java/org/softwire/training/bookish/models/database/Book.java
+++ src/main/java/org/softwire/training/bookish/models/database/Book.java
@@ -2,4 +2,60 @@ package org.softwire.training.bookish.models.database;
 
 public class Book {
 
+    private String BookID;
+    private String title;
+    private String author;
+    private String isbn;
+    private String category;
+
+    public String getBookID() {
+        return BookID;
+    }
+
+    public void setBookID(String bookID) {
+        BookID = bookID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "BookID='" + BookID + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", category='" + category + '\'' +
+                '}';
+    }
 }
